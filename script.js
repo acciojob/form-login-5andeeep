e579c3e27550125319a53cfec7d03772bd4d8be2
Sandeep <98701264+5andeeep@users.noreply.github.com>
2023-09-21 13:19:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const form = document.getElementById('form1');
+
 function getFormvalue() {
     //Write your code here
-
+	const firstName = form[0].value;
+	const lastName = form[1].value;
+	alert(`${firstName} ${lastName}`);
 }
